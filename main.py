f1eb57292268c0cb73375c2072adfcd9e99d8c34
alexpodlucky <app5775@psu.edu>
2020-09-18 15:16:26 +0000

    errors

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 def digit_sum(n):
-   if n <= 0:
-       return 0
-   else:
+  if n <= 0:
+    return 0
+  else:
     return ((n % 10) + digit_sum(int(n / 10)))
 def run():
-  num = int(input('Enter an integer : '))
-  print('Sum of digits of '+ str(num) + ' is', int(digit_sum(num)))
+  num = int(input('Enter an int: '))
+  print('sum of digits of '+ str(num) + ' is', int(digit_sum(num) + "."))
   
 
 if __name__ == '__main__':
